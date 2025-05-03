ee73998d4fc54c16f01e5709470f188a28b66b22
André Vasconcellos <andretbv@al.insper.edu.br>
2025-05-03 10:58:38 -0300

    exercicio2_guardardado/versao1

--- funcoes.py
+++ funcoes.py
@@ -7,3 +7,10 @@ def rolar_dados(n):
         lista.append(numero)
         i+=1
     return lista
+
+def guardar_dado(dados_rolados,dados_guardados,dado_para_guardar):
+    for i in range (len(dados_rolados)):
+        if dado_para_guardar==dados_rolados[i]:
+            dados_guardados.append(dado_para_guardar)
+    return dados_guardados
+
